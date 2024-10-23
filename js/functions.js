3423021478860787ad03991204e99d88c1e9e8ab
Ольга Чулакова <olka.13@list.ru>
2024-10-23 15:32:27 +0300

    исправляет и добавляет функции

--- js/functions.js
+++ js/functions.js
@@ -1,29 +1,60 @@
-const checkLengthString = function(string, maxLength) {
+// Первая функция
+function checkLengthString(string, maxLength) {
   if (string.length <= maxLength) {
     return true;
-  } else {
+  } 
     return false;
-  }
 };
+
 console.log(checkLengthString('проверяемая строка', 18));
 
-const reverse = function(string) {
-  let revstr = '';
-  for (let i = string.length - 1; i >= 0; i--) {
-    revstr += string[i];
-  }
-  return revstr;
-};
-const polydrom = function(string) {
-  revstr = reverse(string);
-  if (revstr === string) {
-    return true;
-  } else {
-    return false;
-  }
-};
-console.log(polydrom('довод'));
+// Строка короче 20 символов
+checkLengthString('проверяемая строка', 20); // true
+// Длина строки ровно 18 символов
+checkLengthString('проверяемая строка', 18); // true
+// Строка длиннее 10 символов
+checkLengthString('проверяемая строка', 10); // false
 
+// Вторая функция - первый вариант решения
+function isReverse(string) {
+  let revStr = '';
+  for (let i = string.length - 1; i >= 0; i--) {
+    revStr += string[i];
+  }
+  return revStr;
+};
+function isPolydrom(string) {
+  string = string.replaceAll(' ', '').toLowerCase();
+  revStr = isReverse(string);
+  if (revStr === string) {
+    return true;
+  }
+    return false;
+};
+
+console.log(isPolydrom('довОд'));
+ 
+// Строка является палиндромом
+isPolydrom('топот'); // true
+// Несмотря на разный регистр, тоже палиндром
+isPolydrom('ДовОд'); // true
+// Это не палиндром
+isPolydrom('Кекс');  // false
+// Это палиндром
+isPolydrom('Лёша на полке клопа нашёл '); // true
+
+
+// Вторая функция - второй вариант решения
+function isPolydrom(string) {
+const cleaned = string.replaceAll(' ', '').toLowerCase();
+const reversed = cleaned.split('').reverse().join('');
+
+return cleaned === reversed;
+};
+
+console.log(isPolydrom('Лёша на полке клопа нашёл '));
+
+// Третья функция - до конца не решена!!!
 function extractNumbers(str) {
   let numbers = '';
   for (let i = 0; i < str.length; i++) {
